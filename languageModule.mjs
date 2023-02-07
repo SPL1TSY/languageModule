f9f549b01442bff4d0f471a212235b0a572211a8
Harun Hurtic <harunhurtic@gmail.com>
2023-02-07 20:04:42 +0100

    languageModule

---
+++ languageModule.mjs
@@ -0,0 +1,11 @@
+// Add an object with translated text for each language
+const translations = {
+    en: {
+      translation: "Translation shows up here",
+    },
+    no: {
+        translation: "Oversettelse kommer her",
+        }
+  };
+  
+  export default translations;
